93582e180fafe3dfed6d4d368c68ab73902fb816
Rafael <fifinhatua@gmail.com>
2023-11-25 14:24:35 -0300

    fix: consertei o erro

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function processaCadastroUsuario(requisicao, resposta) {
 
 
 //indicando para a aplicação como servir arquivos estáticos localizados na pasta 'paginas'
-app.use(express.static(path.join(process.cwd(),'paginas')));
+app.use(express.static(path.join(process.cwd(),'Paginas')));
 
 app.get('/', (requisicao, resposta) => {
     resposta.end(`
